1fbe2bd824ebee46e6092b29888cfa2978482de9
LetsJessCode <letsjesscode@gmail.com>
2020-10-30 19:55:35 -0400

    added about section, and nav

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About Me..</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati deserunt assumenda voluptatem praesentium veritatis fugit aliquid consequuntur inventore, incidunt at, hic quis impedit corporis fugiat sed doloribus ipsa expedita.</p>
+            </div>
+        )
+    }
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import About from './About'
+import Nav from 'react-dom'
 export class App extends Component {
   render() {
     return (
       <div>
         <h1>Dimetri's Website</h1>
+        <About />
       </div>
     )
   }
