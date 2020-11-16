c0046d1caed37f7aed4da62996898fec07644c6e
LetsJessCode <letsjesscode@gmail.com>
2020-11-16 13:44:28 -0500

    changed to SPA...started working on scroll for navigation

--- src/App.js
+++ src/App.js
@@ -4,21 +4,39 @@ import NavBar from './Components/NavBar'
 import Listen from './Components/Listen'
 import Contact from './Components/Contact'
 import TV from './Components/TV'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 export class App extends Component {
 
   render() {
     return (
-    <Router>
       <div className="App">
         <NavBar />
-         <Switch>
-           <Route exact path="/listen" component= {Listen}/>
-           <Route exact path="/contact" component= {Contact}  />
-         </Switch>
+        <About
+          title="Bio"
+          subtitle={About}
+          dark={true}
+          id="about"
+        />
+        <TV
+          title="TV/Film"
+          subtitle={TV}
+          dark={false}
+          id="film"
+        />
+         <Listen
+          title="Section 3"
+          subtitle={Listen}
+          dark={true}
+          id="section3"
+        />
+        <Contact
+          title="Section 4"
+          subtitle={Contact}
+          dark={false}
+          id="section4"
+        />
       </div>
-    </Router>
     )
   }
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { Link, animateScroll as scroll } from "react-scroll";
+import Dimetri from '../Img/Dimetri.jpg'
 
 export class NavBar extends Component {
     render() {
         return (
-            <nav>
-            <Link to="/">Bio</Link> <br />
-            <Link to="/">TV/Film</Link> <br />
-             <NavLink to="/contact">Contact</NavLink> <br />
-             <NavLink to="/listen">Listen</NavLink> <br />
-            </nav>
+    <nav className="nav" id="navbar">
+        <div className="nav-content">
+          <img
+            src={Dimetri}
+            className="nav-logo"
+            alt="Logo."
+            onClick={this.scrollToTop}/>
+          
+            <ul className="nav-items">
+                <Link activeClass="active"
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}> Bio </Link> <br />
+
+                    <Link activeClass="active"
+                    to="film"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}> TV/Film </Link> 
+                    <br />
+
+                    <Link activeClass="active"
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}> Contact </Link> 
+                    <br />
+                    
+                    <Link activeClass="active"
+                    to="listen"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}> Listen </Link> 
+                    <br />
+                {/* <Link >TV/Film /></Link>><br /> */}
+                {/* <NavLink to="/contact">Contact</NavLink> <br /> */}
+                {/* <NavLink to="/listen">Listen</NavLink> <br /> */}
+            </ul>
+        </div>
+    </nav>
         )
     }
 }
