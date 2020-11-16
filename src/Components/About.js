6535b3432febd4135512f693974be073c7dab0c2
LetsJessCode <letsjesscode@gmail.com>
2020-11-16 12:37:24 -0500

    added logos

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ export class About extends Component {
         return (
             <div className="about">
                 <h1>About</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati deserunt assumenda voluptatem praesentium veritatis fugit aliquid consequuntur inventore, incidunt at, hic quis impedit corporis fugiat sed doloribus ipsa expedita.</p>
+                <p>Dimetri always had a passion and drive for music for as long as he can remember.  At the age of 17, he bought his first Bass Guitar, and locked himself in his room for that entire summer and taught himself to play the bass. One of his biggest musical inspirations early on, was Jaco Pastorius. He got his start playing for various churches, as time progressed his ear grew along with his drive.  With every turn in life, it brought him to new and exciting opportunities, allowing him to expand his craft. ...</p>
             </div>
         )
     }
--- src/Components/TV.js
+++ src/Components/TV.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-
+import ESPN from '../Img/ESPN.png'
+import metro from '../Img/metro.png'
+import netflix from '../Img/netflix.jpg'
 export class TV extends Component {
     render() {
         return (
             <div>
                 <h1>Placements</h1>
-                <li>placements and their logos</li>
+                <img src={ESPN} alt=""/>
+                <img src={metro} alt=""/>
+                <img src={netflix} alt="" />
             </div>
         )
     }
